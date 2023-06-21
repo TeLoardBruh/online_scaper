a250edb72dbbba504fbb0fbc0eba94b42421143d
TeLoardBruh <damnloardbruh@gmail.com>
2023-06-21 14:55:50 +0700

    feat: do map with lon and lat

--- cambodia_realestate/Real_Estate.py
+++ cambodia_realestate/Real_Estate.py
@@ -1,10 +1,21 @@
+import folium
+from streamlit_folium import st_folium
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({
-        "lat" : [11.5400445],
-        "lon" : [104.95696239350958]
-    })
+df = pd.read_csv('realestate_kh_cleaner2023-06-21.csv')
+df
 
-st.map(df)
+df = df.dropna(subset=['lat', 'lon'])
+
+data_lat_list = df['lat'].to_list()
+data_lon_list = df['lon'].to_list()
+
+df = pd.DataFrame(
+    {
+        'lat': data_lat_list,
+        'lon': data_lon_list,
+    },
+)
+st.map(df,zoom=2,use_container_width=True)
